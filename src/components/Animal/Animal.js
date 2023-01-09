c13071a403356259235f74991a04480349f34f04
Tom Booth <tombeezle@gmail.com>
2023-01-09 14:48:57 -0800

    styled posts

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -2,7 +2,7 @@ import './Animal.css';
 
 export default function Animal(props) {
   return (
-    <div className="animal">
+    <div className="animal" style={{ top: props.top, left: props.left }}>
       <img src={`animals/${props.images}.svg`} />
       <h2 className="name">{props.name}</h2>
       <p>{props.says}</p>
